e0cd18a4ec731bad5eec8c0d64d7864391a25fe7
Sylvan Franklin <sylvanfranklin@icloud.com>
2024-08-25 21:41:36 -0400

    alias change, and change to wm conf

--- fish/functions/op.fish
+++ fish/functions/op.fish
@@ -1,3 +1,4 @@
-function op
-    ~/op.sh
+function op --wraps='zathura (find . -name "*.pdf" | sk)' --description 'alias op=zathura (find . -name "*.pdf" | sk)'
+  zathura (find . -name "*.pdf" | sk) $argv
+        
 end
