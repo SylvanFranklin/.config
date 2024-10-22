75a35fd3f015b14e57e5638e92d25898bb0e0c9a
Sylvan Franklin <sylvanfranklin@icloud.com>
2024-10-22 15:08:30 -0400

    scripting changes for open pdf

--- scripts/op.sh
+++ scripts/op.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
+
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/downloads ~/documents ~/documents/notes -mindepth 1 -maxdepth 3 -name "*.pdf" | sk)
+    dir=$(tmux run "echo #{pane_start_path}")
+    selected=$(find $dir ~/downloads ~/documents/notes/EDU -mindepth 1 -maxdepth 1 -name "*.pdf" | \
+        sed "s|^$HOME/||" | \
+        sk --margin 10% --color=bw)
+
+    # Add home path back
+    if [[ -n "$selected" ]]; then
+        selected="$HOME/$selected"
+    fi
 fi
 
+if [[ -z "$selected" ]]; then
+    exit 1
+fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 tmux new-window -n  $selected_name -d zathura $selected
+
+
+sleep 0.4
+aerospace focus --dfs-index 1 && aerospace move-node-to-workspace P --focus-follows-window && aerospace macos-native-fullscreen
--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/documents/projects ~/ ~/documents/cs2300  ~/documents/notes/EDU ~/documents/notes -mindepth 1 -maxdepth 1 -type d | sk)
+    selected=$(find ~/documents/projects ~/ ~/documents/cs2300  ~/documents/notes/EDU ~/documents/notes -mindepth 1 -maxdepth 1 -type d | sk --margin 10% --color="bw")
 fi
 
 if [[ -z $selected ]]; then
