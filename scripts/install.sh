44f82118c9c38e763cda1ea12e1b510b0bf5c80c
SylvanFranklin <sylvanfranklin@icloud.com>
2024-11-06 12:02:07 -0500

    added install script

---
+++ scripts/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+apps=(
+"fish",
+"fd",
+"fzf",
+"gh",
+"lazygit",
+"meson",
+"node",
+"ripgrep",
+"sk",
+"tmux",
+)
+
+casks=(
+"aerospace",
+"alacritty",
+"discord",
+"figma",
+"firefox",
+"font-0xproto-nerd-font",
+"font-3270-nerd-font",
+"font-agave-nerd-font",
+"font-fira-code-nerd-font",
+"karabiner-elements",
+"keepassxc",
+"raycast",
+"spotify",
+"tor-browser",
+)
+
+for app in "${apps[@]}"; do
+  echo "Installing $app..."
+  brew install "$app"
+done
+
+for cask in "${casks[@]}"; do
+  echo "Installing cask $cask..."
+  brew install --cask "$cask"
+done
+
+echo "All applications installed!"
