abc7b87c47a6d3be7b00295aaadff09a387e4f4c
SylvanFranklin <sylvanfranklin@icloud.com>
2024-11-18 15:24:56 -0500

    small script for switching to header files

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -77,7 +77,7 @@ vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", { silent = true })
 vim.keymap.set("n", "<leader>g", ":Telescope live_grep<CR>", { silent = true })
 vim.keymap.set("n", "<leader>r", ":Telescope oldfiles<CR>", { silent = true })
-vim.keymap.set("n", "<leader>b", ":Telescope buffers<CR>", { silent = true })
+-- vim.keymap.set("n", "<leader>b", ":Telescope buffers<CR>", { silent = true })
 
 -- lsp commands
 vim.keymap.set({ "n", "x" }, "<leader>lf", ":lua format()<CR>", { silent = true })
@@ -108,3 +108,40 @@ vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>")
 -- sorting by dates (s-d)
 vim.keymap.set({ "v", "x" }, "<leader>s", ":!sort -M<CR>")
 
+
+
+function jump_header()
+    -- Get the full path of the current file
+    local file = vim.fn.expand("%:p")
+    local ext = vim.fn.expand("%:e")
+
+    -- Define potential source and header file extensions
+    local source_exts = { "cpp", "c" }
+    local header_exts = { "h", "hpp", "hh" }
+
+    -- Determine the new extension
+    local target_exts = nil
+    if vim.tbl_contains(header_exts, ext) then
+        target_exts = source_exts
+    elseif vim.tbl_contains(source_exts, ext) then
+        target_exts = header_exts
+    else
+        print("Not a recognized C++ header or source file.")
+        return
+    end
+
+    -- Attempt to find and open the counterpart file
+    local base_name = vim.fn.expand("%:r")
+    for _, target_ext in ipairs(target_exts) do
+        local target_file = base_name .. "." .. target_ext
+        if vim.fn.filereadable(target_file) == 1 then
+            vim.cmd("edit " .. target_file)
+            return
+        end
+    end
+
+    print("Corresponding file not found.")
+end
+
+-- Key mapping to jump between header and source files
+vim.keymap.set("n", "<leader>b", ":lua jump_header()<CR>", { silent = true })
