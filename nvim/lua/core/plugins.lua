1cf32ffde8ab1640a3dc5f133795b63c23b44149
SylvanFranklin <sylvanfranklin@icloud.com>
2024-12-07 12:02:07 -0500

    plugins update

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -23,6 +23,17 @@ require("lazy").setup({
         branch = "harpoon2",
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    -- {
+    --     "otavioschwanck/arrow.nvim",
+    --     dependencies = {
+    --         { "nvim-tree/nvim-web-devicons" },
+    --     },
+    --     opts = {
+    --         show_icons = true,
+    --         leader_key = ';', -- Recommended to be a single key
+    --         buffer_leader_key = 'm', -- Per Buffer Mappings
+    --     }
+    -- },
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
