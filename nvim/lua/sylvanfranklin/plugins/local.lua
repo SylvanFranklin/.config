d7f79a8a55240249599bca4bc9ece41cf9cb000d
SylvanFranklin <sylvanfranklin@icloud.com>
2025-01-17 12:41:25 -0500

    state: local plugins

--- nvim/lua/sylvanfranklin/plugins/local.lua
+++ nvim/lua/sylvanfranklin/plugins/local.lua
@@ -1,5 +1,13 @@
 return {
-    "omni-preview.nvim",
-    dir = "~/documents/projects/omni-preview.nvim",
-    opts = {}
+    { "toppair/peek.nvim",            lazy = true }, -- markdown
+    { 'chomosuke/typst-preview.nvim', lazy = true }, -- typst
+    { 'hat0uma/csvview.nvim',         lazy = true }, -- csv
+    {
+        "omni-preview",
+        dir = "~/documents/projects/omni-preview.nvim",
+        config = function()
+            require("omni-preview").setup()
+            vim.keymap.set("n", "<leader>p", ":OmniPreviewToggle<CR>", { silent = true })
+        end
+    }
 }
