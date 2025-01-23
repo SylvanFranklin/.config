136d7572b3d40b915edea660883058f2a2a3d7cb
SylvanFranklin <sylvanfranklin@icloud.com>
2025-01-23 10:39:43 -0500

    feat: zenmode restyle

--- nvim/lua/sylvanfranklin/keys.lua
+++ nvim/lua/sylvanfranklin/keys.lua
@@ -17,23 +17,22 @@ vim.keymap.set({ "n", "v" }, "<leader>d", '"+d')
 -- vim.keymap.set({ "n", "x", "v" }, "<right>", "<nop>")
 
 function toggle_aesthetic()
-    -- function that toggles the aesthetic of the editor, goes into zen mode
     require("zen-mode").toggle({
         window = {
             backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-            width = 0.85, -- width of the Zen window
-            height = 0.9, -- height of the Zen window
+            width = 1,    -- width of the Zen window
+            height = 1,   -- height of the Zen window
             -- by default, no options are changed for the Zen window
             -- uncomment any of the options below, or add other vim.wo options you want to apply
-            -- options = {
-            --     signcolumn = "no",     -- disable signcolumn
-            --     number = true,         -- disable number column
-            --     relativenumber = true, -- disable relative numbers
-            --     cursorline = false,    -- disable cursorline
-            --     cursorcolumn = false,  -- disable cursor column
-            --     foldcolumn = "0",      -- disable fold column
-            --     list = false,          -- disable whitespace characters
-            -- },
+            options = {
+                signcolumn = "yes",     -- disable signcolumn
+                number = false,         -- disable number column
+                relativenumber = false, -- disable relative numbers
+                cursorline = false,     -- disable cursorline
+                cursorcolumn = false,   -- disable cursor column
+                foldcolumn = "0",       -- disable fold column
+                list = true,            -- disable whitespace characters
+            },
         },
 
         plugins = {
@@ -50,3 +49,39 @@ function toggle_aesthetic()
 end
 
 vim.keymap.set("n", "<leader>u", ":lua toggle_aesthetic()<CR>")
+
+function jump_header()
+    -- Get the full path of the current file
+    -- local file = vim.fn.expand("%:p")
+    local ext = vim.fn.expand("%:e")
+
+    -- Define potential source and header file extensions
+    local source_exts = { "cpp", "c" }
+    local header_exts = { "h", "hpp", "hh" }
+
+    -- Determine the new extension
+    local target_exts = nil
+    if vim.tbl_contains(header_exts, ext) then
+        target_exts = source_exts
+    elseif vim.tbl_contains(source_exts, ext) then
+        target_exts = header_exts
+    else
+        print("Not a recognized C++ header or source file.")
+        return
+    end
+
+    -- Attempt to find and open the counterpart file
+    local base_name = vim.fn.expand("%:r")
+    for _, target_ext in ipairs(target_exts) do
+        local target_file = base_name .. "." .. target_ext
+        if vim.fn.filereadable(target_file) == 1 then
+            vim.cmd("edit " .. target_file)
+            return
+        end
+    end
+
+    print("Corresponding file not found.")
+end
+
+-- Key mapping to jump between header and source files
+vim.keymap.set("n", "<leader>b", ":lua jump_header()<CR>", { silent = true })
