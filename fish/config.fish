215cc41e60c7857048f5613656a08eab3556f0ad
SylvanFranklin <sylvanfranklin@icloud.com>
2025-01-31 12:21:56 -0500

    feat: haskell

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,4 @@ end
 starship init fish | source
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /Users/sylvanfranklin/.ghcup/bin # ghcup-env
+fish_add_path /Users/sylvanfranklin/.modular/bin
