99792d6222e7f9de76dc4888893544f8c22f3cad
SylvanFranklin <sylvanfranklin@icloud.com>
2025-02-05 09:22:45 -0500

    feat: docs reader

--- nvim/lua/sylvanfranklin/plugins/colors.lua
+++ nvim/lua/sylvanfranklin/plugins/colors.lua
@@ -1,10 +1,16 @@
-return {
+return { {
     "vague2k/vague.nvim",
     config = function()
         require("vague").setup({ transparent = true })
         vim.cmd("colorscheme vague")
         vim.cmd(":hi statusline guibg=NONE")
     end
+},
+    -- For `plugins.lua` users.
+    {
+        "OXY2DEV/helpview.nvim",
+        lazy = false
+    },
 
     -- "rktjmp/lush.nvim",
     -- if you wish to use your own colorscheme:
