c0f6f714429c2e39c18efca4df144d0c22bc3243
SylvanFranklin <sylvanfranklin@icloud.com>
2025-02-07 18:24:46 -0500

    comment out

--- nvim/lua/sylvanfranklin/plugins/trouble.lua
+++ nvim/lua/sylvanfranklin/plugins/trouble.lua
@@ -1,33 +1,27 @@
 return {
-    "folke/trouble.nvim",
-    -- opts = {
-    --     -- win = {
-    --     --     type = "floating"
-    --     -- }
-    --     warn_no_results = "false",
+    -- "folke/trouble.nvim",
+    -- cmd = "Trouble",
+    -- keys = {
+    --     -- {
+    --     --     "<leader>lq",
+    --     --     "<cmd>Trouble diagnostics toggle<cr>",
+    --     --     desc = "Diagnostics (Trouble)",
+    --     -- },
+    --     -- {
+    --     --     "<leader>ls",
+    --     --     "<cmd>Trouble symbols toggle focus=false<cr>",
+    --     --     desc = "Symbols (Trouble)",
+    --     -- },
+    --     -- {
+    --     --     "<leader>la",
+    --     --     "<cmd>Trouble qflist toggle<cr>",
+    --     --     desc = "Quickfix List (Trouble)",
+    --     -- },
     -- },
-    cmd = "Trouble",
-    keys = {
-        {
-            "<leader>lq",
-            "<cmd>Trouble diagnostics toggle<cr>",
-            desc = "Diagnostics (Trouble)",
-        },
-        {
-            "<leader>ls",
-            "<cmd>Trouble symbols toggle focus=false<cr>",
-            desc = "Symbols (Trouble)",
-        },
-        {
-            "<leader>la",
-            "<cmd>Trouble qflist toggle<cr>",
-            desc = "Quickfix List (Trouble)",
-        },
-    },
-    config = function()
-        require("trouble").setup {
-            focus = true,
-            warn_no_results = false,
-        }
-    end
+    -- config = function()
+    --     require("trouble").setup {
+    --         focus = true,
+    --         warn_no_results = false,
+    --     }
+    -- end
 }
