88340f0fb86493a67c81ba40a66fb9e8ee92fc62
SylvanFranklin <sylvanfranklin@icloud.com>
2025-02-07 18:24:36 -0500

    feat: default config

--- nvim/lua/sylvanfranklin/plugins/zenmode.lua
+++ nvim/lua/sylvanfranklin/plugins/zenmode.lua
@@ -1,33 +1,30 @@
-return
-{
+return {
     "folke/zen-mode.nvim",
-    opts = {
-        window = {
-            backdrop = 1, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-            width = 0.8,  -- width of the Zen window
-            height = 0.8, -- height of the Zen window
-            -- by default, no options are changed for the Zen window
-            -- uncomment any of the options below, or add other vim.wo options you want to apply
-            options = {
-                signcolumn = "yes",     -- disable signcolumn
-                number = false,         -- disable number column
-                relativenumber = false, -- disable relative numbers
-                cursorline = false,     -- disable cursorline
-                cursorcolumn = false,   -- disable cursor column
-                foldcolumn = "0",       -- disable fold column
-                list = true,            -- disable whitespace characters
-            },
-        },
-
-        plugins = {
-            options = {
-                enabled = true,
-                ruler = false,         -- disables the ruler text in the cmd line area
-                showcmd = false,       -- disables the command in the last line of the screen
-                laststatus = 0,        -- turn off the statusline in zen mode
-            },
-            tmux = { enabled = true }, -- disables the tmux statusline
-        }
-
-    }
+    -- opts = {
+    --     window = {
+    --         backdrop = 1,               -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+    --         width = 0.8,                -- width of the Zen window
+    --         height = 0.8,               -- height of the Zen window
+    --         options = {
+    --             signcolumn = "yes",     -- disable signcolumn
+    --             number = false,         -- disable number column
+    --             relativenumber = false, -- disable relative numbers
+    --             cursorline = false,     -- disable cursorline
+    --             cursorcolumn = false,   -- disable cursor column
+    --             foldcolumn = "0",       -- disable fold column
+    --             list = true,            -- disable whitespace characters
+    --         },
+    --     },
+    --
+    --     plugins = {
+    --         options = {
+    --             enabled = true,
+    --             ruler = false,         -- disables the ruler text in the cmd line area
+    --             showcmd = false,       -- disables the command in the last line of the screen
+    --             laststatus = 0,        -- turn off the statusline in zen mode
+    --         },
+    --         tmux = { enabled = true }, -- disables the tmux statusline
+    --     }
+    --
+    -- }
 }
