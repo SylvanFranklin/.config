9404b7ff8fb7632374d50a393b6234b390e29451
SylvanFranklin <sylvanfranklin@icloud.com>
2025-08-05 08:31:55 -0500

    feat: current implementation

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,18 @@
-vim.opt.tabstop = 4
+vim.opt.winborder = "rounded"
+vim.opt.tabstop = 2
 vim.opt.cursorcolumn = false
 vim.opt.ignorecase = true
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.swapfile = false
 vim.opt.termguicolors = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.incsearch = true
 vim.opt.signcolumn = "yes"
+
 local map = vim.keymap.set
 
 vim.g.mapleader = " "
@@ -22,25 +22,21 @@ map('n', '<leader>q', ':quit<CR>')
 map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 map({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
-
+map({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
-	{ src = "https://github.com/L3MON4D3/LuaSnip" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = 'https://github.com/NvChad/showkeys',                opt = true },
 })
 
 require "mason".setup()
+require "showkeys".setup({ position = "top-right" })
 require "mini.pick".setup()
-require "nvim-treesitter.configs".setup({
-	ensure_installed = { "svelte", "typescript", "javascript", "css" },
-	highlight = { enable = true }
-})
 require "oil".setup()
 
 map('n', '<leader>f', ":Pick files<CR>")
@@ -49,23 +45,8 @@ map('n', '<leader>e', ":Oil<CR>")
 map('t', '', "")
 map('t', '', "")
 map('n', '<leader>lf', vim.lsp.buf.format)
-vim.lsp.enable({ "lua_ls", "svelte", "tinymist", "emmetls" })
 
-vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {
-			runtime = { version = 'LuaJIT', },
-			diagnostics = { globals = { 'vim', 'require' },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-})
+vim.lsp.enable({ "lua_ls", "svelte", "tinymist", "emmetls" })
 
 -- colors
 require "vague".setup({ transparent = true })
