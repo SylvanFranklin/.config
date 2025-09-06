aaa939c71e6a771e2a5d384ab0460202cffefeae
SylvanFranklin <sylvanfranklin@icloud.com>
2025-09-06 00:18:46 -0400

    feat: time logging script

--- scripts/time.sh
+++ scripts/time.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 CATEGORIES=(
-	"MATH"
-	"WORKFLOW"
-	"VIDEO"
-	"PROGRAMMING"
-	"DRAWING"
+    "MATH"
+    "WORKFLOW"
+    "STORMY"
+    "VIDEO"
+    "PROGRAMMING"
+    "DRAWING"
+    "WASTED"
+    "STOP"
 )
 
 selected=$(printf "%s\n" "${CATEGORIES[@]}" | sk --margin 10% --color="bw")
 
-timew start $selected
+if [[ "$selected" == "STOP" ]]; then
+    timew stop
+		tmux set -g status-right ""
+else
+    timew start "$selected"
+		tmux set -g status-right "$selected "
+fi
+
