2032f59f158a78a3f27c4d88e0b5e0854291a034
SylvanFranklin <sylvanfranklin@icloud.com>
2025-10-06 09:41:19 -0400

    extra insructions

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+git clone https://github.com/sylvanfranklin/.config
+cd .config
+brew bundle
+
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.Dock autohide -bool true
+defaults write com.apple.Dock autohide-delay -float 1000
+defaults write com.apple.universalaccess reduceMotion -bool true
+
+killall SystemUIServer
+killall Dock
