4cccdaea366bf1acc5ff761c42f4c511a702dbc3
helios <adamdungbell@outlook.com>
2026-01-02 20:19:24 +0000

    Remove unnecessary blank lines in debug.lua

--- nvim/debug.lua
+++ nvim/debug.lua
@@ -1,9 +1,6 @@
 require("nvim-dap-virtual-text").setup()
 local dap, dapui = require("dap"), require("dapui")
 
-
-
-
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
 end
