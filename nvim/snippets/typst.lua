5d452674d2c2c9995f6de3b678c97d3f00245b8c
SylvanFranklin <sylvanfranklin@icloud.com>
2026-01-13 11:53:35 -0500

    new typst snippes

--- nvim/snippets/typst.lua
+++ nvim/snippets/typst.lua
@@ -17,8 +17,18 @@ return {
 	s({ trig = "mmt", snippetType = "autosnippet" },
 		fmta("$ <> $ ", { i(1) })
 	),
+	s({ trig = "i" },
+		fmt("==>", {})
+	),
 	s({ trig = "cent" },
 		fmta("#align(center)[<>]", { i(1) })
+	),
+	s({ trig = "pi" },
+		fmta([[
+_Proof_: We use induction on $n$. \
+Base case: For $n=1$, $<>$ \
+Inductive Step: Suppose now as inductive hypothesis that $<>$. Then since <>
+		]], { i(1), i(2), i(3) })
 	),
 	s({ trig = "v" },
 		fmta("#let <> = <>", { i(1), i(2) })
