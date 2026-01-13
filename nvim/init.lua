935d189b4e7caec7c22bfc15e194c871a928ac59
SylvanFranklin <sylvanfranklin@icloud.com>
2026-01-13 11:53:43 -0500

    debugging

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,11 +32,26 @@ vim.pack.add({
 	{ src = "https://github.com/mfussenegger/nvim-dap" },
 	{ src = "https://github.com/rcarriga/nvim-dap-ui" },
 	{ src = "https://github.com/theHamsta/nvim-dap-virtual-text" },
-	{ src = "https://github.com/julianolf/nvim-dap-lldb" }
+	{ src = "https://github.com/julianolf/nvim-dap-lldb" },
+	{ src = "https://github.com/nvim-neotest/nvim-nio" }
 })
 
 
-require("debug")
+require("dap-lldb").setup()
+local dap, dapui = require("dap"), require("dapui")
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+vim.keymap.set({ 'n' }, '<Leader>d', ':DapNew<CR>')
+vim.keymap.set({ 'n', 'i' }, '<C-b>', ':DapToggleBreakpoint<CR>')
 require "marks".setup {
 	builtin_marks = { "<", ">", "^" },
 }
@@ -167,7 +182,7 @@ local current = 1
 vim.g.mapleader = " "
 
 map({ "n", "x" }, "<leader>y", '"+y')
-map({ "n", "x" }, "<leader>d", '"+d')
+-- map({ "n", "x" }, "<leader>d", '"+d')
 map({ "i", "s" }, "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
 map({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
 map({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
