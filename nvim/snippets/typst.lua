7050fc32ce4e8ec05194a0d11684d76c5db07ede
SylvanFranklin <sylvanfranklin@icloud.com>
2026-03-05 12:19:23 -0500

    remove dead snippets

--- nvim/snippets/typst.lua
+++ nvim/snippets/typst.lua
@@ -14,18 +14,6 @@ return {
 	s({ trig = "cent" },
 		fmta("#align(center)[<>]", { i(1) })
 	),
-	s({ trig = "p" },
-		fmta([[_Proof_: $quad$ <>
-
-		]], { i(1) })
-	),
-	s({ trig = "pi" },
-		fmta([[
-_Proof_: We use induction on $n$. \
-Base case: For $n=1$, $<>$ \
-Inductive Step: Suppose now as inductive hypothesis that $<>$. Then since <>
-		]], { i(1), i(2), i(3) })
-	),
 	s({ trig = "v" },
 		fmta("#let <> = <>", { i(1), i(2) })
 	),
