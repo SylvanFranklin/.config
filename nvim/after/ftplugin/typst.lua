e5b97e14f44b655ab8d7160ec840419a281c131f
SylvanFranklin <sylvanfranklin@icloud.com>
2026-03-05 12:19:16 -0500

    inner outer textobject delete

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,6 +1,27 @@
 vim.keymap.set("n", "<leader>p", ":TypstPreview<CR>", { buffer = 0 })
 
 
+-- configuration
+require("nvim-treesitter-textobjects").setup {
+	select = {
+		lookahead = true,
+		selection_modes = {
+			['@math.inner'] = 'v',
+			['@math.outer'] = 'V',
+		},
+		include_surrounding_whitespace = false,
+	},
+}
+
+-- keymaps
+-- You can use the capture groups defined in `textobjects.scm`
+vim.keymap.set({ "x", "o" }, "am", function()
+	require "nvim-treesitter-textobjects.select".select_textobject("@math.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "im", function()
+	require "nvim-treesitter-textobjects.select".select_textobject("@math.inner", "textobjects")
+end)
+
 vim.cmd([[
 	"setlocal wrapmargin=10
 	"setlocal formatoptions+=t
